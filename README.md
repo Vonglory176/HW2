3583ddef286c7fcaeb1b16174652b0078c24d9d6
LukeD <locdaohuuit@outlook.com>
2023-01-12 23:01:53 -0800

    [LocD] Calculate average 3 numbers

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HW2
 My 2nd HW for Prog 209 - Use CSS in some interesting ways
+
+P1 - Conley, Skyler	
+P2 - Dao, Loc
--- main.js
+++ main.js
@@ -1,8 +1,53 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-
-    document.getElementById("button1").addEventListener("click", function () {
-
-        document.getElementById("textbox1").value = "success";
-    });
+document.addEventListener("DOMContentLoaded", function(_) {
+    document.getElementById("average-button").addEventListener("click", handleOnClickAverageButton)
 });
 
+
+function calculateAverage(intArr) {
+    if (intArr === null || intArr === undefined || intArr.length <= 0) {
+        throw new Error("Invalid number array");
+    }
+
+    let result = 0;
+    intArr.forEach(function(num) {
+        result += num;
+    });
+
+    return result / intArr.length;
+}
+
+function parseStringToInt(stringValue) {
+    if (isNaN(stringValue)) {
+        throw new Error(stringValue + " is not a number");
+    }
+
+    return parseInt(stringValue);
+}
+
+function getValueFromElement(elementId) {
+    return document.getElementById(elementId).value;
+}
+
+function displayResult(resultAsString) {
+    console.log(resultAsString);
+    document.getElementById("result-content").innerHTML = resultAsString;
+}
+
+function handleOnClickAverageButton(_) {
+    try {
+        const firstNumberValue = getValueFromElement("first-number");
+        const secondNumberValue = getValueFromElement("second-number");
+        const thirdNumberValue = getValueFromElement("third-number");
+
+        let intArr = [
+            parseStringToInt(firstNumberValue),
+            parseStringToInt(secondNumberValue),
+            parseStringToInt(thirdNumberValue)
+        ];
+
+        const averageResult = calculateAverage(intArr);
+        displayResult(averageResult);
+    } catch (e) {
+        displayResult(e.message);
+    }
+}
