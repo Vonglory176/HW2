54acf143d7c9e5127017b1d5f5142c6cb36cd538
LukeD <loc.daobc@bellevuecollege.edu>
2023-01-13 23:11:00 -0800

    [LocD] put some comments

--- main.js
+++ main.js
@@ -2,21 +2,26 @@ document.addEventListener("DOMContentLoaded", function(_) {
     document.getElementById("average-button").addEventListener("click", handleOnClickAverageButton)
 });
 
-
-function calculateAverage(intArr) {
-    if (intArr === null || intArr === undefined || intArr.length <= 0) {
+/*
+ * Input: Array of numbers 
+ * Output: Return average number of array
+ * */
+function calculateAverage(numberArr) {
+    // Throw an exception whenever array of number is not defined or is empty
+    if (numberArr === null || numberArr === undefined || numberArr.length <= 0) {
         throw new Error("Invalid number array");
     }
 
     let result = 0;
-    intArr.forEach(function(num) {
+    numberArr.forEach(function(num) {
         result += num;
     });
 
-    return result / intArr.length;
+    return result / numberArr.length;
 }
 
 function parseStringToFloat(stringValue) {
+    // Throw an exception when string is not a number
     if (isNaN(stringValue)) {
         throw new Error(stringValue + " is not a number");
     }
@@ -30,12 +35,13 @@ function getValueFromElement(elementId) {
 
 function displayResult(resultAsString) {
     console.log(resultAsString);
+    document.getElementById("result-content").style.display = "block";
     document.getElementById("result-content").innerHTML = resultAsString;
 
-    //CHANGES HERE: Adding CSS to output
-    document.getElementById("result-content").style.backgroundColor="lightgrey";
-    document.getElementById("result-content").style.borderStyle="dotted";
-    document.getElementById("result-content").style.margin="0px 50px 0px 50px"; //top,right,bottom,left
+    // CHANGES HERE: Adding CSS to output
+    document.getElementById("result-content").style.backgroundColor = "lightgrey";
+    document.getElementById("result-content").style.borderStyle = "dotted";
+    document.getElementById("result-content").style.margin = "0px 50px 0px 50px"; //top,right,bottom,left
 }
 
 function handleOnClickAverageButton(_) {
@@ -44,15 +50,16 @@ function handleOnClickAverageButton(_) {
         const secondNumberValue = getValueFromElement("second-number");
         const thirdNumberValue = getValueFromElement("third-number");
 
-        let intArr = [
+        let numberArr = [
             parseStringToFloat(firstNumberValue),
             parseStringToFloat(secondNumberValue),
             parseStringToFloat(thirdNumberValue)
         ];
 
-        const averageResult = calculateAverage(intArr);
+        const averageResult = calculateAverage(numberArr);
         displayResult(averageResult);
     } catch (e) {
+        // whenever an exception get caught, we will display error message
         displayResult(e.message);
     }
 }
